35cf37fd063f2afb2d085d35a63f2f871625cb00
LorenaMondragon <40040525+LorenaMondragon@users.noreply.github.com>
2024-05-12 13:32:23 -0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+import inputTag from "./components/inputTag.vue";
+
+export default{
+  components:{
+    inputTag,
+  },
+  methods: {
+    handleOnTagsChange(tags){
+      console.log(tags)
+    },
+  }
+};
+</script>
+
+<template>
+  <input-tag :onTagsChange="handleOnTagsChange"/>
+</template>
+
+<style scoped>
+
+</style>
